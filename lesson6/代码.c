c022fcea5f3a5eb8f54efb4f79aaaf1c8e8c86c2
whisper-y <900589058@qq.com>
2020-06-21 22:50:37 +0800

    这是第五次作业 by 闫振兴-19020100330

---
+++ lesson6/代码.c
@@ -0,0 +1,23 @@
+
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup() {
+  lcd.begin(16, 2); //初始化为1602
+  lcd.print("hello, world!");
+  lcd.setCursor(0, 1);
+  lcd.print("input()");
+  Serial.begin(9600) ;
+}
+
+void loop() 
+{
+	if (Serial.available() > 0 )
+    {
+      	lcd.setCursor(0, 1);
+      	lcd.print(Serial.readString()) ;
+    }
+
+}
+ 
